537069d7d4e6bff1b10021b442425baea06593da
fernando-roldan <fernando_roldanf@hotmail.com>
2024-03-15 13:19:50 -0600

    app

---
+++ app/src/main/java/com/example/rickandmorty/ui/detail/components/DetailProperty.kt
@@ -0,0 +1,39 @@
+package com.example.rickandmorty.ui.detail.components
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun DetailProperty(
+    modifier: Modifier = Modifier,
+    label: String,
+    value: String?,
+    imageVector: ImageVector
+) {
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(vertical = 10.dp, horizontal = 4.dp),
+        elevation = 3.dp,
+        backgroundColor = Color(0xFFFFFFFF)
+    ) {
+        Row(modifier = Modifier.padding(8.dp)) {
+            Spacer(Modifier.width(10.dp))
+            Column {
+                Text(text = label, fontWeight = FontWeight.Bold, color = Color.Black)
+                Spacer(Modifier.height(5.dp))
+                Text(text = value ?: "",  color = Color.Black, style = MaterialTheme.typography.caption)
+            }
+        }
+    }
+}
